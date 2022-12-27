f466f494c52a4c9d08426bcc6031c1dfb7745ee1
Sylvan Butler <gitstuff.sylvan@xoxy.net>
2022-12-27 13:02:09 -0700

    decrypt the store

--- yahoofinancials/__init__.py
+++ yahoofinancials/__init__.py
@@ -57,8 +57,19 @@ import requests
 from gzip import decompress as gzipdecompress
 from zlib import decompress as zlibdecompress
 
+# encrypted responses 20221221
+import hashlib
+from base64 import b64decode
+try:
+    from Cryptodome.Cipher import AES
+    from Cryptodome.Util.Padding import unpad
+except ImportError:
+    # Cryptodome installed by another name
+    from Crypto.Cipher import AES
+    from Crypto.Util.Padding import unpad
+
 # meh
-VERSTR = '1.1'
+VERSTR = '1.2'
 
 # log information about failed requests - get or parse failures
 LOG_FAILURES = True
@@ -151,6 +162,69 @@ def _be_nice(activitycb):
         time.sleep(max(0, now - time.time()))
     _lastget = now
     activitycb(0)
+
+
+# decrypt encrypted responses 20221221
+# from https://github.com/ranaroussi/yfinance/issues/1246#issuecomment-1356709536
+def _decrypt(data):
+
+    def EVPKDF(
+        password,
+        salt,
+        keySize=32,
+        ivSize=16,
+        iterations=1,
+        hashAlgorithm="md5",
+    ) -> tuple:
+        """OpenSSL EVP Key Derivation Function
+        Args:
+            password (Union[str, bytes, bytearray]): Password to generate key from.
+            salt (Union[bytes, bytearray]): Salt to use.
+            keySize (int, optional): Output key length in bytes. Defaults to 32.
+            ivSize (int, optional): Output Initialization Vector (IV) length in bytes. Defaults to 16.
+            iterations (int, optional): Number of iterations to perform. Defaults to 1.
+            hashAlgorithm (str, optional): Hash algorithm to use for the KDF. Defaults to 'md5'.
+        Returns:
+            key, iv: Derived key and Initialization Vector (IV) bytes.
+        Taken from: https://gist.github.com/rafiibrahim8/0cd0f8c46896cafef6486cb1a50a16d3
+        OpenSSL original code: https://github.com/openssl/openssl/blob/master/crypto/evp/evp_key.c#L78
+        """
+        assert iterations > 0, "Iterations can not be less than 1."
+        if isinstance(password, str):
+            password = password.encode("utf-8")
+        final_length = keySize + ivSize
+        key_iv = b""
+        block = None
+        while len(key_iv) < final_length:
+            hasher = hashlib.new(hashAlgorithm)
+            if block:
+                hasher.update(block)
+            hasher.update(password)
+            hasher.update(salt)
+            block = hasher.digest()
+            for _ in range(1, iterations):
+                block = hashlib.new(hashAlgorithm, block).digest()
+            key_iv += block
+        key, iv = key_iv[:keySize], key_iv[keySize:final_length]
+        return key, iv
+
+    encrypted = b64decode(data['context']['dispatcher']['stores'])
+    assert encrypted[:8] == b"Salted__"
+    salt = encrypted[8:16]
+    encrypted = encrypted[16:]
+
+    _cs = data["_cs"]
+    _crdata = loads(data["_cr"])
+    _crwords = _crdata["words"]
+    _cr = b"".join(int.to_bytes(i, length=4, byteorder="big", signed=True) for i in _crwords)
+    assert _crdata["sigBytes"] == len(_cr)
+
+    password = hashlib.pbkdf2_hmac("sha1", _cs.encode("utf8"), _cr, 1, dklen=32).hex()
+    key, iv = EVPKDF(password, salt, keySize=32, ivSize=16, iterations=1, hashAlgorithm="md5")
+    cipher = AES.new(key, AES.MODE_CBC, IV=iv)
+    plaintext = cipher.decrypt(encrypted)
+    plaintext = unpad(plaintext, 16, style="pkcs7")
+    return loads(plaintext)
 
 
 def time_report(f, *a, **kwa):
@@ -404,6 +478,8 @@ class YahooFinanceETL(object):
         data = loads(re.search("root.App.main\s+=\s+(\{.*\})", script).group(1))
         try:
             self._cache[url] = data["context"]["dispatcher"]["stores"]
+            if self._cache[url][:3] == 'U2F':
+                self._cache[url] = _decrypt(data)
         except Exception as e:
             # no point in trying this over and over again so remember the parse failure
             self._cache[url] = ParseException(
